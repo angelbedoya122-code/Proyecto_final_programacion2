4802c9c291154df788087d34f06c917c6c18d950
Angel Bedoya <angelbedoya122@gmail.com>
2026-05-25 19:58:58 +0000

    "Agregadas soluciones 3 y 4 parcial 1 por Bedoya"

--- parcial1/soluciones_p1.py
+++ parcial1/soluciones_p1.py
@@ -12,3 +12,14 @@ def f2(L):
         L3 = [x[0]*x[1] for x in list(zip(L, L2))]
         return sum(L3)
     return evaluar
+
+def f3(x0, y0):
+    def recta(x):
+        return 2 * (x - x0) + y0  
+    def paralela(x):
+        return 2 * (x - 1) + 1 
+    return recta, paralela
+
+def f4(L):
+    L = [x for x in L if x > 10 and (int(str(x)[0]) + int(str(x)[-1])) % 2 == 0]
+    return sum(L)
